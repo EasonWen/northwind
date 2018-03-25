3d71cf0fb82d4b9dd76ef663519099efa0b3cb11
LOVEE\eason.wen <jossisgood@gmail.com>
2018-03-26 00:12:22 +0800

    # create order API

---
+++ NorthwindAppService/NorthwindAppService/Controllers/OrderController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindAppService.DAL;
+
+namespace NorthwindAppService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Order")]
+    public class OrderController : Controller
+    {
+        //private CustomersDAO _customersDAO;
+        private OrdersDAO _ordersDAO;
+
+        public OrderController()
+        {
+            //_customersDAO = new CustomersDAO();
+            _ordersDAO = new OrdersDAO();
+        }
+
+
+        // GET: api/Order/10248
+        [HttpGet("{ID}")]
+        public IActionResult Get(int ID)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var data = _ordersDAO.Get(ID);
+
+                if (data == null)
+                    return NotFound(data);
+
+                return Ok(data);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        // POST: api/Order/10248
+        [HttpPost]
+        public IActionResult PostOrder([FromBody]Orders order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var orderId = _ordersDAO.Create(order);
+                if (orderId > 0)
+                {
+                    return Created($"/api/Order/{orderId}", orderId);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        // PUT: api/Order/10248
+        [HttpPut("{ID}")]
+        public IActionResult PutOrder(int ID, [FromBody]Orders order)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (ID != order.OrderID)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _ordersDAO.Update(order);
+            }
+            catch (Exception)
+            {
+                if (!OrderExists(ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+
+        }
+
+        // DELETE: api/Order/5
+        [HttpDelete("{ID}")]
+        public IActionResult DeleteOrder(int ID)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var data = _ordersDAO.Get(ID);
+
+                if (data == null)
+                    return NotFound(data);
+
+                return Ok(_ordersDAO.Delete(ID));
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
+        private bool OrderExists(int ID)
+        {
+            return _ordersDAO.IsExists(ID);
+        }
+    }
+}
---
+++ NorthwindAppService/NorthwindAppService/DAL/DAO/CustomersDAO.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindAppService.DAL
+{
+    public class CustomersDAO : DBContext
+    {
+        public CustomersDAO()
+        {
+            connection.Open();
+        }
+
+        public IEnumerable<Customers> GetCustomer()
+        {
+            string str = $"SELECT * FROM dbo.Customers";
+            var query = connection.Query<Customers>(str);
+            connection.Dispose();
+
+            return query;
+        }
+
+
+            public Customers GetCustomer(string ID)
+        {
+            string str = $"SELECT * FROM dbo.Customers WHERE CustomerID ='{ID}' ";
+            var query = connection.Query<Customers>(str).FirstOrDefault();
+            connection.Dispose();
+
+            return query;
+        }
+    }
+}
